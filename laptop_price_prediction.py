8d21ae121ebc8dad1fc87b0b26b7c900ad21c3f1
Poo-wei-chien <u2102771@siswa.um.edy.my>
2023-05-24 20:59:39 +0800

    Improve model to 0.63 for train set and 0.67 for test set

--- laptop_price_prediction.py
+++ laptop_price_prediction.py
@@ -1,49 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
-dfmodel = pd.read_csv('laptop-clean-train.csv')
-dfmodel['screen size'] = dfmodel['screen size'].str.replace('"', '')
-
-# Initialize the label encoder
-cat_cols =  ['manufacturer', 'category', 'cpu', 'gpu', 'operating system',
-            'resolution', 'screen_type', 'storage_1_type', 'storage_2_type',
-            'gpu_brand', 'cpu_brand']
-
-#One hot encoding
-print('Dataframe encoded by OHE dimension : ', pd.get_dummies(dfmodel, columns = cat_cols, drop_first = True).shape)
-
-#Label encoding
-en = LabelEncoder()
-
-for cols in cat_cols:
-    dfmodel[cols] = en.fit_transform(dfmodel[cols])
-
-
-x_train = dfmodel.drop('price', axis=1)
-x_train = x_train.drop('operating system version', axis=1)
-x_train = x_train.drop('model name', axis=1)
-x_train = x_train.drop('cpu_freq(GHz)', axis=1)
-y_train = dfmodel['price']
-
+import pickle
 
 def predict_laptop_price_model(data_input):
-    from sklearn.model_selection import train_test_split
-    from sklearn.linear_model import LinearRegression
-    from sklearn.metrics import mean_squared_error
-    from sklearn.metrics import r2_score
 
-    # Create an instance of LinearRegression
-    model = LinearRegression()
+    # Open the file
+    model = pickle.load(open('laptop_price_prediction_model.pkl', 'rb'))
 
-    # Fit the model on the training data
-    model.fit(x_train, y_train)
-
+    # Make prediction
     return model.predict(data_input)
 
-
-
 label_encoder = LabelEncoder()
-
 
 def predict_laptop_price(manufacturer, category, screen_size, cpu, ram, gpu, operating_system, weight, resolution, screen_type, touchscreen, storage_1_gb, storage_1_type, storage_2_gb, storage_2_type, cpu_brand, gpu_brand):
     # Create a dictionary with the laptop characteristics
@@ -82,7 +49,7 @@ def predict_laptop_price(manufacturer, category, screen_size, cpu, ram, gpu, ope
     predicted_price = predict_laptop_price_model(df.values)
 
     # Change INR to MYR
-    predicted_price[0] = round(predicted_price[0] * 0.0054767, 2)
+    predicted_price[0] = round(predicted_price[0] * 0.00031, 2)
 
     if predicted_price[0] < 0:
         predicted_price[0] = predicted_price[0] * -1    
