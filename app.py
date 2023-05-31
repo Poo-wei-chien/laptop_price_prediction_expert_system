7d9f685118fa01eb7e717ef2ca5089a9b710d05a
Poo-wei-chien <u2102771@siswa.um.edy.my>
2023-06-01 01:13:34 +0800

    done Tuning

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import time
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-from laptop_price_prediction import predict_laptop_price
+from laptop_price_prediction import modelTuning, predict_laptop_price
 import about
 import pandas as pd
 
+#Set up Page Config
 img = Image.open('icon.png')
 st.set_page_config(
     page_title="Laptop Price Prediction Expert System",
@@ -45,49 +46,56 @@ if selected_page == 'Home':
     #Type of laptop
     lap_type = st.selectbox("Type", df['TypeName'].unique())
     #Ram
-    ram = st.selectbox("Ram(in GB)", [2,4,6,8,12,16,24,32,64])
+    ram = st.selectbox("Ram (in GB)", [2,4,6,8,12,16,24,32,64])
     #weight
-    weight = st.number_input("Weight of the Laptop")
+    weight = st.number_input("Weight of the Laptop", min_value=0.5, max_value=5.00, step=0.01)
     #Touch screen
     touchscreen = st.selectbox("TouchScreen", ['No', 'Yes'])
     #IPS
-    ips = st.selectbox("IPS", ['No', 'Yes'])
+    ips = st.selectbox("IPS (In-Plane Switching) monitor ", ['No', 'Yes'])
     #screen size
-    screen_size = st.number_input('Screen Size')
+    screen_size = st.number_input('Screen Size', min_value=10.0, max_value=20.0, step=0.1,format="%.1f")
     # resolution
     resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
     #cpu
     cpu = st.selectbox('CPU',df['Cpu_brand'].unique())
-    hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
-    ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+    hdd = st.selectbox('HDD (in GB)',[0,128,256,512,1024,2048])
+    ssd = st.selectbox('SSD (in GB)',[0,8,128,256,512,1024])
     gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
     os = st.selectbox('OS',df['os'].unique())
 
     if st.button('SUBMIT'):
         with st.spinner('Wait for it...'):
-            time.sleep(2)
-        
-        #Prediction
-        ppi = None
-        if touchscreen == "Yes":
-            touchscreen = 1
+            time.sleep(0.1)
+        if(hdd == 0 and ssd == 0):
+            st.error('Please enter the HDD, SSD or both of the laptop')
+            st.stop()
+        elif(company == 'Apple' and os != 'Mac'):
+            st.error('Please enter the OS as macOS for Apple laptops')
+            st.stop()
+        elif(company != 'Apple' and os == 'Mac'):
+            st.error('Please enter the OS as Windows, Linux or others for non-Apple laptops')
+            st.stop()
         else:
-            touchscreen = 0
+            #Prediction
+            ppi = None
+            if touchscreen == "Yes":
+                touchscreen = 1
+            else:
+                touchscreen = 0
+                
+            if ips == "Yes":
+                ips = 1
+            else:
+                ips = 0
+                
+            X_res = int(resolution.split('x')[0])
+            Y_res = int(resolution.split('x')[1])
+            ppi = ((X_res ** 2) + (Y_res**2)) ** 0.5 / screen_size
+            prediction = predict_laptop_price(company,lap_type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os)
+            prediction = modelTuning(prediction, company,lap_type,ram,touchscreen,ips,cpu,hdd,ssd,gpu,os)
+            st.title("The predicted price of this configuration is RM " + str(prediction))
             
-        if ips == "Yes":
-            ips = 1
-        else:
-            ips = 0
-            
-        X_res = int(resolution.split('x')[0])
-        Y_res = int(resolution.split('x')[1])
-        ppi = ((X_res ** 2) + (Y_res**2)) ** 0.5 / screen_size
-        prediction = predict_laptop_price(company,lap_type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os)
-        st.title("The predicted price of this configuration is " + str(prediction))
-            
-        # else:
-        #     st.warning("Please enter all the features of the laptop to get the price prediction. '*' cannot be 'None'.")
-
 
 if selected_page == 'About':
     # About page content
--- laptop_price_prediction.py
+++ laptop_price_prediction.py
@@ -45,3 +45,120 @@ def predict_laptop_price(company,lap_type,ram,weight,touchscreen,ips,ppi,cpu,hdd
     predicted_price[0] = round(predicted_price[0] * 0.056, 2)
 
     return predicted_price[0]
+
+def modelTuning(predictedPrice, company,lap_type,ram,touchscreen,ips,cpu,hdd,ssd,gpu,os):
+    totalPredictedPrice = predictedPrice
+    #Tuning "Company" feature
+    if company == 'Apple':
+        totalPredictedPrice += predictedPrice * 0.30
+    elif company == 'HP':
+        totalPredictedPrice += predictedPrice * 0.10
+    elif company == 'Acer':
+        totalPredictedPrice -= predictedPrice * 0.05
+    elif company == 'Asus':
+        totalPredictedPrice += predictedPrice * 0.05
+    elif company == 'Dell':
+        totalPredictedPrice += predictedPrice * 0.20
+    elif company == 'Lenovo':
+        totalPredictedPrice += predictedPrice * 0.10
+    elif company == 'Chuwi':
+        totalPredictedPrice -= predictedPrice * 0.15
+    elif company == 'MSI':
+        totalPredictedPrice += predictedPrice * 0.15
+    elif company == 'Microsoft':
+        totalPredictedPrice += predictedPrice * 0.20
+    elif company == 'Toshiba':
+        totalPredictedPrice -= predictedPrice * 0.05
+    elif company == 'Huawei':
+        totalPredictedPrice -= predictedPrice * 0.10
+    elif company == 'Xiaomi':
+        totalPredictedPrice -= predictedPrice * 0.10
+    elif company == 'Vero':
+        totalPredictedPrice -= predictedPrice * 0.15
+    elif company == 'Razer':
+        totalPredictedPrice += predictedPrice * 0.25
+    elif company == 'Mediacom':
+        totalPredictedPrice -= predictedPrice * 0.15
+    elif company == 'Samsung':
+        totalPredictedPrice += predictedPrice * 0.15
+    elif company == 'Google':
+        totalPredictedPrice += predictedPrice * 0.15
+    elif company == 'Fujitsu':
+        totalPredictedPrice += predictedPrice * 0.05
+    elif company == 'LG':
+        totalPredictedPrice += predictedPrice * 0.10
+
+    #Tuning "Type" feature
+    if lap_type == 'Netbook':
+        totalPredictedPrice -= predictedPrice * 0.10
+    elif lap_type == 'Gaming':
+        totalPredictedPrice += predictedPrice * 0.20
+    elif lap_type == '2 in 1 Convertible':
+        totalPredictedPrice += predictedPrice * 0.10
+    elif lap_type == 'Workstation':
+        totalPredictedPrice += predictedPrice * 0.30
+
+    #Tuning "Ram" feature
+    if ram == 4:
+        totalPredictedPrice += predictedPrice * 0.10
+    elif ram == 6:
+        totalPredictedPrice += predictedPrice * 0.30
+    elif ram == 8:
+        totalPredictedPrice -= predictedPrice * 0.15
+    elif ram == 16:
+        totalPredictedPrice -= predictedPrice * 0.10
+    elif ram == 32:
+        totalPredictedPrice += predictedPrice * 0.20
+    elif ram == 64:
+        totalPredictedPrice += predictedPrice * 0.50
+    
+    #Tuning "Touchscreen" feature
+    if touchscreen == 1:
+        totalPredictedPrice += predictedPrice * 0.20
+
+    #Tuning "IPS" feature
+    if ips == 1:
+        totalPredictedPrice += predictedPrice * 0.15
+
+    #Tuning "CPU" feature
+    if cpu == 'Intel Core i7':
+        totalPredictedPrice += predictedPrice * 0.25
+    elif cpu == 'AMD Processor':
+        totalPredictedPrice -= predictedPrice * 0.15
+    elif cpu == 'Intel Core i3':
+        totalPredictedPrice -= predictedPrice * 0.15
+
+    #Tuning "HDD" feature
+    if hdd == 256:
+        totalPredictedPrice += predictedPrice * 0.10
+    elif hdd == 512:
+        totalPredictedPrice += predictedPrice * 0.15
+    elif hdd == 1024:
+        totalPredictedPrice += predictedPrice * 0.25
+    elif hdd == 2048:
+        totalPredictedPrice += predictedPrice * 0.30
+
+    #Tuning "SSD" feature
+    if ssd == 8:
+        totalPredictedPrice -= predictedPrice * 0.40
+    elif ssd == 256:
+        totalPredictedPrice += predictedPrice * 0.10
+    elif ssd == 512:
+        totalPredictedPrice += predictedPrice * 0.15
+    elif ssd == 1024:
+        totalPredictedPrice += predictedPrice * 0.30
+
+    #Tuning "GPU" feature
+    if gpu == 'AMD':
+        totalPredictedPrice -= predictedPrice * 0.15
+    elif gpu == 'Nvidia':
+        totalPredictedPrice += predictedPrice * 0.15
+
+    #Tuning "OS" feature
+    if os == 'Mac':
+        totalPredictedPrice += predictedPrice * 0.15
+    elif os == 'Others/No OS/Linux':
+        totalPredictedPrice -= predictedPrice * 0.15
+
+    return totalPredictedPrice
+    
